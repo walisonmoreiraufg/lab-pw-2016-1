4afe3187b398e1b1a73bbb2740538472bb2b2b96
Walison Moreira <walison.moreira@gmail.com>
2016-05-10 21:06:24 -0300

    MVC.

--- servlet-jsp/src/main/java/servletjsp/CalculadoraServlet.java
+++ servlet-jsp/src/main/java/servletjsp/CalculadoraServlet.java
@@ -3,12 +3,13 @@ package servletjsp;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-@WebServlet(value="/calculadora.php")
+@WebServlet(value="/calculadora.php") // ;)
 public class CalculadoraServlet extends 
 	HttpServlet {
 
@@ -18,8 +19,17 @@ public class CalculadoraServlet extends
 			HttpServletRequest req,
 			HttpServletResponse resp) throws ServletException, IOException {
 
-		resp.getOutputStream().print("<html><head><title>Calculadora</title></head><body><h1>Calculadora</h1><b>2 + 2 = " + (2 + 2) + "</b></body></html>");
+		ServletOutputStream out = resp.getOutputStream();
 		
+		out.print("<html>");
+		out.print("<head>");
+		out.print("<title>Calculadora</title>");
+		out.print("</head>");
+		out.print("<body>");
+		out.print("<h1>Calculadora</h1>");
+		out.print("<b>2 + 2 = " + (2 + 2) + "</b>");
+		out.print("</body>");
+		out.print("</html>");
 	}
 	
 }
